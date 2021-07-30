3eeed1db8b08ccc1bcf30a6b09c35ec40dc9c627
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2021-07-30 10:49:28 +0530

    Update movie.java

--- movie.java
+++ movie.java
@@ -1,18 +1,16 @@
 package JavaDB_001;
 import javax.swing.*;
-import java.sql.Connection;  
-import java.sql.DriverManager;  
-import java.sql.SQLException;  
+import java.sql.*;
 import java.awt.event.*;
 
 public class movie extends JFrame implements ActionListener{
     JLabel JL_actor,JL_actress,JL_year,JL_movie,JL_director;
     JTextField JT_actor,JT_actress,JT_year,JT_movie,JT_director;
-    JButton btn_search;
+    JButton btn_search,btn_insert;
 
 
     public movie(){
-        super("Search movies");
+        super("movies");
         JL_movie = new JLabel("Enter movie name:");
         JL_movie.setBounds(20, 20, 200, 20);
         JT_movie = new JTextField(20);
@@ -20,10 +18,21 @@ public class movie extends JFrame implements ActionListener{
         btn_search = new JButton("Search");
         btn_search.setBounds(300, 20, 80, 20);
         btn_search.addActionListener(this);
+
+        btn_insert = new JButton("Insert");
+        btn_insert.setBounds(300, 60, 80, 20);
+        btn_insert.addActionListener(this);
+
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setSize(500,200);
+
+
+        JL_director = new JLabel(" Director name: ");
+        JL_director.setBounds(20, 140, 100, 20);
+        JT_director = new JTextField(20);
+        JT_director.setBounds(130, 140, 150, 20);
 
         JL_actor = new JLabel(" Actor name: ");
         JL_actor.setBounds(20, 50, 100, 20);
@@ -39,6 +48,7 @@ public class movie extends JFrame implements ActionListener{
         JT_year.setBounds(130, 110, 150, 20);
         setLayout(null);
         add(btn_search);
+        add(btn_insert);
         add(JL_actor);
         add(JT_actor);
         add(JL_actress);
@@ -47,6 +57,8 @@ public class movie extends JFrame implements ActionListener{
         add(JT_year);
         add(JL_movie);
         add(JT_movie);
+        add(JL_director);
+        add(JT_director);
     }
 
     @Override
@@ -56,6 +68,7 @@ public class movie extends JFrame implements ActionListener{
         String fn = "actor";
         String ln = "actress";
         String ag = "year";
+        String am= "director";
 
         rs = f.find(JT_movie.getText());
         try{
@@ -77,11 +90,12 @@ public class movie extends JFrame implements ActionListener{
         PreparedStatement ps = null;
         public ResultSet find(String s){
             try{
-                 String url = "jdbc:sqlite:C:/sqlite/movies.db";  
-                  conn = DriverManager.getConnection(url); 
-                  System.out.println("Connection to SQLite has been established."); 
+                con = DriverManager.getConnection("jdbc:mysql://localhost/test_db","root","");
+                ps = con.prepareStatement("select * from users where id = ?");
+                ps.setString(1,s);
+                rs = ps.executeQuery();
             }catch(Exception ex){
-                System.out.println(e.getMessage()); 
+                JOptionPane.showMessageDialog(null, ex.getMessage());
             }
             return rs;
         }
@@ -94,4 +108,4 @@ public class movie extends JFrame implements ActionListener{
     }
 
 }
-
+/////////////////// END :)
