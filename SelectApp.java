c779a60aa553a546c63ce2eb8f6e3b6c96caddcd
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2021-07-30 10:24:34 +0530

    Update SelectApp.java

--- SelectApp.java
+++ SelectApp.java
@@ -1,49 +1,96 @@
-package net.sqlitetutorial;
+package JavaDB_001;
+import javax.swing.*;
+import java.sql.*;
+import java.awt.event.*;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+public class Work extends JFrame implements ActionListener{
+    JLabel JL_actor,JL_actress,JL_year,JL_movie,JL_director;
+    JTextField JT_actor,JT_actress,JT_year,JT_movie,JT_director;
+    JButton btn_search;
 
-public class SelectApp {
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C:/sql/movies.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
+
+    public Work(){
+        super("Search movies");
+        JL_movie = new JLabel("Enter movie name:");
+        JL_movie.setBounds(20, 20, 200, 20);
+        JT_movie = new JTextField(20);
+        JT_movie.setBounds(130, 20, 150, 20);
+        btn_search = new JButton("Search");
+        btn_search.setBounds(300, 20, 80, 20);
+        btn_search.addActionListener(this);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(500,200);
+
+        JL_actor = new JLabel(" Actor name: ");
+        JL_actor.setBounds(20, 50, 100, 20);
+        JT_actor = new JTextField(20);
+        JT_actor.setBounds(130, 50, 150, 20);
+        JL_actress = new JLabel("Actress name: ");
+        JL_actress.setBounds(20, 80, 100, 20);
+        JT_actress = new JTextField(20);
+        JT_actress.setBounds(130, 80, 150, 20);
+        JL_year = new JLabel("Year: ");
+        JL_year.setBounds(20, 110, 100, 20);
+        JT_year = new JTextField(20);
+        JT_year.setBounds(130, 110, 150, 20);
+        setLayout(null);
+        add(btn_search);
+        add(JL_actor);
+        add(JT_actor);
+        add(JL_actress);
+        add(JT_actress);
+        add(JL_year);
+        add(JT_year);
+        add(JL_movie);
+        add(JT_movie);
     }
 
-    public void selectAll(){
-        String sql = "SELECT name,actor,actress,director,year FROM movies";
-        
-        try (Connection conn = this.connect();
-             Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sql)){
-            
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(
-                                   rs.getString("name") + "\t" +
-				   rs.getString("actor") + "\t" +
-				   rs.getString("actress") + "\t" +
-			           rs.getString("director") + "\t" +
-				   rs.getInt("year"));
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Function f = new Function();
+        ResultSet rs = null;
+        String fn = "actor";
+        String ln = "actress";
+        String ag = "year";
+
+        rs = f.find(JT_movie.getText());
+        try{
+            if(rs.next()){
+                JT_actor.setText(rs.getString("actor"));
+                JT_actress.setText(rs.getString("actress"));
+                JT_year.setText(rs.getString("year"));
+                JT_year.setText(rs.getString("year"));
+            }  else{
+                JOptionPane.showMessageDialog(null, "NO DATA FOR THIS ID");
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage());
         }
     }
-    
+    public class Function{
+        Connection con = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        public ResultSet find(String s){
+            try{
+                con = DriverManager.getConnection(""jdbc:sqlite:C:/sql/movies.db","root","");
+                ps = con.prepareStatement("select * from users where id = ?");
+                ps.setString(1,s);
+                rs = ps.executeQuery();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+            return rs;
+        }
 
-    public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+    }
+
+
+    public static void main(String[] args){
+        new Work();
     }
 
 }
+
